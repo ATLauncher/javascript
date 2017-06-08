e92993745489ff8b6f413e9cedbc889e55fd627d
Ryan Dowling <ryan@ryandowling.me>
2017-06-08 21:34:41 +1000

    chore: move to babel-plugin-env to compile to node 8

--- packages/babel-preset-atlauncher/index.js
+++ packages/babel-preset-atlauncher/index.js
@@ -3,9 +3,13 @@
 module.exports = function() {
     return {
         presets: [
-            require('babel-preset-es2015'),
+            [require('babel-preset-env'), {
+                targets: {
+                    node: 8,
+                },
+            }],
             require('babel-preset-stage-0'),
-            require('babel-preset-react')
+            require('babel-preset-react'),
         ],
         plugins: [
             require('babel-plugin-transform-runtime'),
