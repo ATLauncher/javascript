9e859f978b92c350f0c9b1356b2e8fe9a74ece81
Ryan Dowling <ryan@ryandowling.me>
2017-08-19 22:03:39 +1000

    feat(atlauncher-scripts): add in noConcurrency option for test

--- packages/atlauncher-scripts/scripts/test.js
+++ packages/atlauncher-scripts/scripts/test.js
@@ -1,27 +1,26 @@
 const utils = require('../utils');
 const colors = require('colors/safe');
 
-// Do this as the first thing so that any code reading it knows the right env.
+// do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'test';
 process.env.NODE_ENV = 'test';
 
-// Makes the script crash on unhandled rejections instead of silently
+// makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
 // terminate the Node.js process with a non-zero exit code.
-process.on('unhandledRejection', err => {
+process.on('unhandledRejection', (err) => {
     throw err;
 });
 
 (() => {
     const args = process.argv.slice(2);
-    let testFramework = utils.getConfigFromPackageJson('testFramework');
-    let coverageFramework = utils.getConfigFromPackageJson('coverageFramework');
     let watch = false;
     let debug = false;
     let coverage = false;
+    let noConcurrency = false;
 
     if (args.length) {
-        args.forEach(arg => {
+        args.forEach((arg) => {
             if (arg === '--watch') {
                 watch = true;
             }
@@ -33,12 +32,16 @@ process.on('unhandledRejection', err => {
             if (arg === '--debug') {
                 debug = true;
             }
+
+            if (arg === '--noConcurrency') {
+                noConcurrency = true;
+            }
         });
     }
 
     const processArguments = [];
 
-    let command = [utils.getNodeModulesBinPath('jest')];
+    const command = [utils.getNodeModulesBinPath('jest')];
 
     if (watch) {
         command.push('--watch');
@@ -52,12 +55,15 @@ process.on('unhandledRejection', err => {
         command.push('--coverage');
     }
 
+    if (noConcurrency) {
+        command.push('--runInBand');
+    }
+
     processArguments.push(command);
 
     if (!processArguments.length) {
-        console.error(
-            colors.red('Error processing input. Please check your arguments and try again.'),
-        );
+        console.error(colors.red('Error processing input. Please check your arguments and try again.'));
+
         return;
     }
 
